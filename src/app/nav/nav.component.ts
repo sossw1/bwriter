634b065f01acca24b08a4d0e8ee11f57e36595e2
sossw1 <sossw1@gmail.com>
2021-10-27 10:21:09 -0400

    nav component

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bw-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
