44fd1b3df562f6e601a7f9c7cf83df06913a97ad
sossw1 <sossw1@gmail.com>
2021-11-03 14:45:29 -0400

    pass data to note list items

--- src/app/note-list-item/note-list-item.component.ts
+++ src/app/note-list-item/note-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'bw-note-list-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-list-item.component.css']
 })
 export class NoteListItemComponent implements OnInit {
+  @Input() note;
 
   constructor() { }
 
