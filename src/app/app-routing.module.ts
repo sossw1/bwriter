d7920e8460e25f6e8b52a9f65bb59fd01c2e6b5c
sossw1 <sossw1@gmail.com>
2021-11-04 11:11:30 -0400

    default path shows note component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteComponent } from './note/note.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NoteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
