d6d7e97bf680cd138259c4fb4cd682e9eb2e0e15
sossw1 <sossw1@gmail.com>
2021-11-18 16:22:16 -0500

    integrate firebase realtime database and hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { NavComponent } from './nav/nav.component';
 import { NoteComponent } from './note/note.component';
 import { NoteListComponent } from './note-list/note-list.component';
 import { NoteListItemComponent } from './note-list-item/note-list-item.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { NoteListItemComponent } from './note-list-item/note-list-item.component
     NoteListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bwriter-92393',
+    appId: '1:464582639045:web:15609219712efe26201fac',
+    storageBucket: 'bwriter-92393.appspot.com',
+    apiKey: 'AIzaSyCx_GcUqgWdyusKjArgHXJhELdYCfilJ0w',
+    authDomain: 'bwriter-92393.firebaseapp.com',
+    messagingSenderId: '464582639045',
+  },
   production: false
 };
 
